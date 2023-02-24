05c788ce51ff477e076b6fa21be94e6d571ffc7a
kuhuhalder <kuhuhalder2701@gmail.com>
2023-02-24 18:44:07 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-
+import CreateMovie from './components/CreateMovie';
+import ShowMovieList from './components/ShowMovieList';
+import UpdateMovie from './components/UpdateMovie';
+import ShowMovieDetails from './components/ShowMovieDetails';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path = "/" element = {<ShowMovieList/>} />
+          <Route exact path = "/add" element = {<CreateMovie/>} />
+          <Route exact path = "/edit/:id" element = {<UpdateMovie/>} />
+          <Route exact path = "/show/:id" element = {<ShowMovieDetails/>} />
+
+        </Routes>
+      </div>
+    </Router>    
   );
 }
 
